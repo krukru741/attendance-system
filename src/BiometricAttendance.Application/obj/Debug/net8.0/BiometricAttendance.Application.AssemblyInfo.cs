e3aea4a0951207e1d238b57d58141ffd96766f26
Alben Gacayan <albengacayan486@gmail.com>
2026-08-25 16:14:30 +0800

    chore: ignore build artifacts and temporary files in version control

--- src/BiometricAttendance.Application/obj/Debug/net8.0/BiometricAttendance.Application.AssemblyInfo.cs
+++ src/BiometricAttendance.Application/obj/Debug/net8.0/BiometricAttendance.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BiometricAttendance.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607ef7586b752ab5203800e8709a8c3a4decd87b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e8195b5b610d2d1a21d2563948bf0ad782ffee")]
 [assembly: System.Reflection.AssemblyProductAttribute("BiometricAttendance.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BiometricAttendance.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
